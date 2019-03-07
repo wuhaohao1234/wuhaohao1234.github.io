7248e09e68d051c9d70490eacf5700ee53726168
abu <1611499758@qq.com>
2019-03-07 17:31:40 +0800

    feat(docs): 增加目录

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -8,20 +8,18 @@
             v-for="item in items"
             :key="item.title"
             avatar
-            @click=""
+            @click="dbClick(item.src)"
           >
             <v-list-tile-avatar>
               <v-icon :class="[item.iconClass]">{{ item.icon }}</v-icon>
             </v-list-tile-avatar>
-
             <v-list-tile-content>
               <v-list-tile-title>{{ item.title }}</v-list-tile-title>
               <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
             </v-list-tile-content>
-
             <v-list-tile-action>
-              <v-btn icon ripple>
-                <v-icon color="grey lighten-1">info</v-icon>
+              <v-btn flat icon color="blue lighten-2">
+                <v-icon>thumb_up</v-icon>
               </v-btn>
             </v-list-tile-action>
           </v-list-tile>
@@ -40,28 +38,120 @@ export default class Notes extends Vue {
           {
             icon: 'folder',
             iconClass: 'grey lighten-1 white--text',
-            title: 'Photos',
-            subtitle: 'Jan 9,2014'
+            title: 'Vue文档',
+            subtitle: '对于开发者入门较快的vue教程',
+            src: 'https://wuhaohao1234.github.io/Teaching'
           },
           {
             icon: 'folder',
             iconClass: 'grey lighten-1 white--text',
-            title: 'Phossstos',
-            subtitle: 'Jan 9,2014'
+            title: 'typescript文档',
+            subtitle: '比ts官网简单的文档',
+            src: 'https://wuhaohao1234.github.io/tpescript-docs/#/'
           },
           {
             icon: 'folder',
             iconClass: 'grey lighten-1 white--text',
-            title: 'Phosstos',
-            subtitle: 'Jan 9,2014'
+            title: 'react小书',
+            subtitle: 'react redux入门',
+            src: 'http://huziketang.mangojuice.top/books/react/'
           },
           {
             icon: 'folder',
             iconClass: 'grey lighten-1 white--text',
-            title: 'Phsotos',
-            subtitle: 'Jan 9,2014'
+            title: 'promise迷你书',
+            subtitle: '学习promise深入理解',
+            src: 'http://liubin.org/promises-book/'
+          },
+          {
+            icon: 'folder',
+            iconClass: 'grey lighten-1 white--text',
+            title: 'MDN',
+            subtitle: '比w3c school好的文档,js高级',
+            src: 'https://developer.mozilla.org/zh-CN/'
+          },
+          {
+            icon: 'folder',
+            iconClass: 'grey lighten-1 white--text',
+            title: 'http协议',
+            subtitle: '详细描述http协议，tcp,Ip协议',
+            src: 'https://hit-alibaba.github.io/interview/basic/network/HTTP.html'
+          },
+          {
+            icon: 'folder',
+            iconClass: 'grey lighten-1 white--text',
+            title: 'webpack4',
+            subtitle: 'webpack 官网，入门简单',
+            src: 'https://www.webpackjs.com/'
+          },
+          {
+            icon: 'folder',
+            iconClass: 'grey lighten-1 white--text',
+            title: 'npm基础',
+            subtitle: 'npm详细，发布与使用npm包',
+            src: 'https://my.oschina.net/fenying/blog/1607571'
+          },
+          {
+            icon: 'folder',
+            iconClass: 'grey lighten-1 white--text',
+            title: 'git教程',
+            subtitle: '很好的git教程',
+            src: 'https://my.oschina.net/fenying/blog/1607571'
+          },
+          {
+            icon: 'folder',
+            iconClass: 'grey lighten-1 white--text',
+            title: '前端面试基础',
+            subtitle: '基础篇',
+            src: 'http://blog.poetries.top/FE-Interview-Questions/'
+          },
+          {
+            icon: 'folder',
+            iconClass: 'grey lighten-1 white--text',
+            title: '设计模式',
+            subtitle: '常用的设计模式文档',
+            src: 'https://godbmw.com/design-patterns/'
+          },
+          {
+            icon: 'folder',
+            iconClass: 'grey lighten-1 white--text',
+            title: 'git commit',
+            subtitle: 'git 提交规范',
+            src: 'https://github.com/wuhaohao1234/learncommitizen-'
+          },
+          {
+            icon: 'folder',
+            iconClass: 'grey lighten-1 white--text',
+            title: '持续集成工具',
+            subtitle: 'travis-ci circle-ci',
+            src: 'http://www.ruanyifeng.com/blog/2015/09/continuous-integration.html'
+          },
+          {
+            icon: 'folder',
+            iconClass: 'grey lighten-1 white--text',
+            title: '阮一峰个人博客',
+            subtitle: '较好的博客',
+            src: 'http://www.ruanyifeng.com'
+          },
+          {
+            icon: 'folder',
+            iconClass: 'grey lighten-1 white--text',
+            title: '廖雪峰个人博客',
+            subtitle: 'git python js java教程',
+            src: 'https://www.liaoxuefeng.com/'
           },
         ]
+
+
+  public dbClick(src: string) {
+    window.open(src)
+  }
 }
 </script>
 
+<style scoped>
+a{
+  display: block;
+  text-decoration: none;
+}
+</style>
--- src/components/Partner.vue
+++ src/components/Partner.vue
@@ -1,18 +1,16 @@
 <template>
   <v-timeline>
     <v-timeline-item
-      v-for="n in 4"
-      :key="n"
       color="red lighten-2"
       large
     >
       <template v-slot:opposite>
-        <span>Tus eu perfecto</span>
+        <span>Fcc西安社区</span>
       </template>
       <v-card class="elevation-2">
-        <v-card-title class="headline">Lorem ipsum</v-card-title>
+        <v-card-title class="headline">Fcc</v-card-title>
         <v-card-text>
-          Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
+          FCC 西安社区(freeCodeCamp 西安社区)建立两年以来，我们一直以为西安本土开发者提供一个开放交流的环境为目的，成功举办了近十次线下 Code && Coffee 活动，受到大家较为广泛的好评。这次正逢 FCC 西安社区诞生两周年，在持续酝酿下，我们特地于 FCC 西安社区两周年当日筹办这场【2018 西安 Web 前端交流大会】，为西安本土的前端工程师、开源爱好者、互联网从业人员以及对技术感兴趣的高校学生们提供一场前所未有的前端技术盛宴。此次盛会期待您的参与！
         </v-card-text>
       </v-card>
     </v-timeline-item>
